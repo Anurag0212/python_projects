ed5ff968e22afe699424166f8ae53f49f1a7e693
Anurag Singh <anurag_singh0212@yahoo.com>
2018-03-17 17:05:31 -0400

    updated similarity ratio and close matches for words

--- Data_Dictionary_Using_Python/code/data_dictionary.py
+++ Data_Dictionary_Using_Python/code/data_dictionary.py
@@ -1,4 +1,5 @@
 import json, glob
+from difflib import get_close_matches
 
 filename = glob.iglob("C:/Users/Anurag/Desktop/python/python_projects/Data_Dictionary_Using_Python/data_folder/*.json")
 
@@ -9,7 +10,12 @@ def dictionary(word):
     if word in data:
         return data[word]
     else:
-        return "please check the word again"
+        print("did you mean:", get_close_matches(word,data.keys())[0])
+        yes_or_no = input("Please enter Y or N:")
+        if yes_or_no == "y" or yes_or_no == "Y":
+            return dictionary(get_close_matches(word,data.keys())[0])
+        else:
+            return "please check the word again"
 
 _input = input("please enter your word:").lower()
 output = dictionary(_input)
