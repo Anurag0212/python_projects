24a5deec5dec78a3c9a48ea55476bc5958206932
Anurag Singh <anurag_singh0212@yahoo.com>
2018-03-18 01:33:13 -0400

    Added comments

--- Data_Dictionary_Using_Python/code/data_dictionary.py
+++ Data_Dictionary_Using_Python/code/data_dictionary.py
@@ -1,17 +1,21 @@
 #Importing libraries using import command
+
 import json, glob
 from difflib import get_close_matches
 
 #Storing all the .json files into a variable name
+
 filename = glob.iglob("C:/Users/Anurag/Desktop/python/python_projects/Data_Dictionary_Using_Python/data_folder/*.json")
 
 #writing a function to load data from all the json file into a variable
 #data will be available in the form of dictionary.
+
 for _file in filename:
     data = json.load(open(_file))
 
 #Defining a function which will search for the word from the dictionary
 #It will provide suggestions to the user of the user input is not present in the dictionary
+
 def dictionary(word):
     if word in data.keys():   #Checks for the word in dictionary
         return data[word]
@@ -31,7 +35,9 @@ def dictionary(word):
     else:
         return "Word doesn't exist. Please double check it."
 
+
 #A function which will take the user input and provide its meaning.
+
 def new_input():
     _input = input("please enter your word:").lower()
     output = dictionary(_input)
@@ -41,5 +47,7 @@ def new_input():
     else:
         print(output)
 
+
 #calling of a function to initial the program.
+
 new_input()
